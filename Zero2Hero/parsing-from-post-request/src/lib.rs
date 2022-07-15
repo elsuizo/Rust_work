cc188f071c43ea4e21dfa4084f0b52dcf93d1d33
elsuizo <mnoblia@disroot.org>
2022-07-15 20:39:28 -0300

    update desde la notebook

--- Zero2Hero/parsing-from-post-request/src/lib.rs
+++ Zero2Hero/parsing-from-post-request/src/lib.rs
@@ -2,11 +2,17 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-async fn subscribe() -> HttpResponse {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
--- Zero2Hero/parsing-from-post-request/tests/health_check.rs
+++ Zero2Hero/parsing-from-post-request/tests/health_check.rs
@@ -15,6 +15,25 @@ async fn healt_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subcriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bin random port");
     // guardamos el port que nos ha asignado por el Sistema operativo
